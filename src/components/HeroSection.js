ead8b2364a27d42854d772870561d4353661ca49
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-02-03 01:41:03 +0300

    Complete HeroSection and lesson 1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
-import k from 'react-flags-select'
+import ReactFlagsSelect from 'react-flags-select'
 import { useState } from "react";
+import { FaFacebook } from 'react-icons/fa'
 
 export default function HeroSection() {
 
@@ -19,7 +20,10 @@ export default function HeroSection() {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    cssEase: 'linear'
   };
 
   return (
@@ -45,15 +49,29 @@ export default function HeroSection() {
         </div>
         <div className="w-[400px] rounded-lg bg-gray-100 p-6">
           <h4 className="text-primary-brand-color text-center font-semibold">Giriş yap veya kayıt ol</h4>
-          <div className="flex">
-            <ReactFlagsSelect
-              countries={Object.keys(phones)}
-              customLabels={phones}
-              placeholder="Select Language"
-              onSelect={code => setSelected(code)}
-              selected={selected}
-              className="flags-select"
-            />
+          <div className="grid gap-y-3">
+            <div className="flex gap-x-2">
+              <ReactFlagsSelect
+                countries={Object.keys(phones)}
+                customLabels={phones}
+                placeholder="Select Language"
+                onSelect={code => setSelected(code)}
+                selected={selected}
+                className="flags-select"
+              />
+              <label className="flex-1 relative group block cursor-pointer">
+                <input required className="peer h-14 px-4 border-2 border-gray-200 rounded-lg w-full transition-colors focus:border-primary-brand-color group-hover:border-primary-brand-color outline-none text-sm pt-2" />
+                <span className="absolute top-0 left-0 h-full px-4 flex items-center text-sm text-gray-500 transition-all peer-focus:h-7 peer-focus:text-primary-brand-color peer-focus:text-xs peer-valid:h-7 peer-valid:text-primary-brand-color peer-valid:text-xs">Telefon Numarası</span>
+              </label>
+            </div>
+            <button className="bg-brand-yellow transition-colors hover:bg-primary-brand-color hover:text-brand-yellow h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold text-primary-brand-color">
+              Telefon numarası ile devam et
+            </button>
+            <hr className="h-[1px] bg-gray-300 my-2" />
+            <button className="bg-blue-700 bg-opacity-10 px-4 transition-colors hover:bg-blue-700 hover:text-white h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold text-blue-700">
+              <FaFacebook size={24} />
+              <span className="mx-auto">Facebook ile devam et</span>
+            </button>
           </div>
         </div>
       </div>
