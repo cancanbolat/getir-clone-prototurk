3eac127078550f305776cb6382c2326c9d7ce108
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-02-03 03:53:11 +0300

    Update App.js, Categories and Campaigns

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -1,7 +1,61 @@
-export default function Campaigns() {
+import Slider from "react-slick";
+import { useState, useEffect } from "react";
+import Banners from 'api/banners.json'
+import Title from "./ui/Title";
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+
+
+function NextBtn({ className, style, onClick }) {
   return (
-    <div>
-        Campaigns
+    <button
+      className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`}
+      onClick={onClick}
+    ><IoIosArrowForward size={22} /></button>
+  );
+}
+
+function PrevBtn({ className, style, onClick }) {
+  return (
+    <button
+      className="text-brand-color absolute top-1/2 -left-6 -translate-y-1/2" onClick={onClick}>
+        <IoIosArrowBack size={22} />
+    </button>
+  );
+}
+
+export default function Campaigns() {
+
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+    setBanners(Banners)
+  }, [])
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    cssEase: 'linear',
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+  };
+
+  return (
+    <div className="container mx-auto py-8">
+      <Title>Kampanyalar</Title>
+      <Slider {...settings} className="-mx-4">
+        {banners && banners.map((banner, index) => (
+          <div key={index}>
+            <picture className="block px-4">
+              <img src={banner.image} className="rounded-lg" />
+            </picture>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import categoryData from 'api/categories.json'
+import Category from "./ui/Category";
+import Title from "./ui/Title";
+
 export default function Categories() {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    //istek
+    setTimeout(() => {
+      setCategories(categoryData);
+    }, 1000);
+  }, [])
+
   return (
-    <div>
-        Categories
+    <div className="bg-white py-4">
+      <div className="container mx-auto">
+        <Title>Kategoriler</Title>
+        <div className="grid grid-cols-10">
+          {!categories.length && 'Yükleniyor...'}
+          {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+        </div>
+      </div>
     </div>
   );
 }
