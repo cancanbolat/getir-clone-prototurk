9763a502b547ea375bb80b1bfdb62b2371e0e9fa
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-02-04 04:02:06 +0300

    Complete Cards.js, Add cards.json and update style.css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,23 @@
+import CardsData from 'api/cards.json'
+import { useState, useEffect } from 'react';
+
 export default function Cards() {
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    setCards(CardsData)
+  }, [])
+
   return (
-    <div>
-        Cards
+    <div className='grid grid-cols-3 gap-x-4'>
+      {cards.length && cards.map((card, index) =>
+        <div key={index} className='bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center'>
+          <img src={card.image} className='w-[150px] h-[150px] mb-6' />
+          <h6 className='font-semibold text-lg text-primary-brand-color'>{card.title}</h6>
+          <p className='mt-2 text-sm text-gray-700'>{card.description}</p>
+        </div>
+      )}
     </div>
   );
 }
